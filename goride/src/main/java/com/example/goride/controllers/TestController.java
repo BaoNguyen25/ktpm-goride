9c83638b9bd8392c69f0d6d4b1abbbdb53055445
bao.mnguyen <bao.mnguyen@katalon.com>
2023-07-29 19:59:20 +0700

    Initial commit

---
+++ goride/src/main/java/com/example/goride/controllers/TestController.java
@@ -0,0 +1,42 @@
+package com.example.goride.controllers;
+
+import com.example.goride.models.User;
+import com.example.goride.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/all")
+    public void allAccess() {
+        userRepository.save(new User("Vin","Bao","Bao"));
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
+
+    @GetMapping("/mod")
+    @PreAuthorize("hasRole('MODERATOR')")
+    public String moderatorAccess() {
+        return "Moderator Board.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Board.";
+    }
+}
